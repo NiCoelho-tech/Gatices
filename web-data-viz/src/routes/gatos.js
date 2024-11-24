ef492bf6890386f2a74d6c12a2ba7b899fc87cc4
NiCoelho-tech <nicolas.sampaio@sptech.school>
2024-11-23 21:46:41 -0300

    adicionar a rota para a listagem dos gatos existentes

--- web-data-viz/src/routes/gatos.js
+++ web-data-viz/src/routes/gatos.js
@@ -6,6 +6,10 @@ var gatoController = require("../controllers/gatoController");
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     gatoController.cadastrar(req, res);
-})
+});
+
+router.get("/listar", function(req, res) {
+    gatoController.listar(req, res);
+}) 
 
 module.exports = router;
