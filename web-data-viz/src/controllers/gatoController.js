080678dd5fd915b5223fa13b44234dea9b6120e1
NiCoelho-tech <nicolas.sampaio@sptech.school>
2024-11-24 22:03:26 -0300

    adicionar as funções de listagem e de autenticação para o gato

--- web-data-viz/src/controllers/gatoController.js
+++ web-data-viz/src/controllers/gatoController.js
@@ -4,12 +4,13 @@ function cadastrar(req, res) {
   var nome = req.body.nomeGatoServer;
   var genero = req.body.generoServer;
   var raca = req.body.racaServer;
+  var fkDono = req.body.donoServer;
 
   gatoModel
-    .cadastrar(nome, genero, raca)
+    .cadastrar(nome, genero, raca, fkDono)
     .then(function (resultado) {
       res.json(resultado);
-      console.log(`Nome: ${nome}, Genero: ${genero}, Raca: ${raca}`);
+      console.log(`Nome: ${nome}, Genero: ${genero}, Raca: ${raca}, Dono: ${fkDono}`);
     })
     .catch(function (erro) {
       console.log(erro);
@@ -21,6 +22,48 @@ function cadastrar(req, res) {
     });
 }
 
+function autenticar(req, res) {
+  var idDono = req.body.idDonoServer;
+
+    gatoModel
+      .autenticar(idDono)
+      .then(function (resultadoAutenticar) {
+        console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+        console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+        if (resultadoAutenticar.length == 1) {
+          console.log(resultadoAutenticar);
+
+          res.json({
+            id: resultadoAutenticar[0].id,
+            nome: resultadoAutenticar[0].nome,
+            genero: resultadoAutenticar[0].genero,
+            raca: resultadoAutenticar[0].raca,
+          });
+        } else if (resultadoAutenticar.length == 0) {
+          res.status(403).send("Gato não encontrado!");
+        } else {
+          res.status(403).send("Mais de um gato existente!");
+        }
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o cadastro!  Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+
+function listar(req, res) {
+  gatoModel.listar().then((resultado) => {
+    res.status(200).json(resultado);
+  });
+}
+
 module.exports = {
-  cadastrar
+  cadastrar,
+  autenticar,
+  listar
 };
