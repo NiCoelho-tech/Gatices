5ab07cf731e341c4ebf26087c7a18caa1136f0b9
NiCoelho-tech <nicolas.sampaio@sptech.school>
2024-11-28 22:41:18 -0300

    atualizar o script do banco de dados

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -13,15 +13,20 @@ CREATE TABLE gato (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
     genero ENUM('M', 'F'), 
-    raca VARCHAR(100)
+    raca VARCHAR(100),
+    fkDono INT,
+    CONSTRAINT fkDonoGato FOREIGN KEY (fkDono) REFERENCES usuario(idUsuario)
 );
 
 CREATE TABLE status_gato (
     id INT PRIMARY KEY AUTO_INCREMENT,
     fkGato INT,
+    fkDono INT,
     fome INT,  -- valor que representa o nível de fome (pode ser de 0 a 100, por exemplo)
     sede INT,  -- valor que representa o nível de sede (pode ser de 0 a 100, por exemplo)
     dtHora TIMESTAMP,
-    CONSTRAINT fkGatoStatus FOREIGN KEY (fkGato) REFERENCES gato(id)
+    status_vida ENUM('VIVO', 'MORTO') DEFAULT 'VIVO',
+    CONSTRAINT fkGatoStatus FOREIGN KEY (fkGato) REFERENCES gato(id),
+    CONSTRAINT fkDonoStatus FOREIGN KEY (fkDono) REFERENCES usuario(idUsuario)
 ); 
 
