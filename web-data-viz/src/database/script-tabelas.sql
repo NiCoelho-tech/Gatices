56f8a4732253ca99a83af2efdee2890390badc81
NiCoelho-tech <nicolas.sampaio@sptech.school>
2024-11-28 22:59:32 -0300

    adicionar o backlog e documentação atualizados

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -10,22 +10,24 @@ CREATE TABLE usuario(
 );
 
 CREATE TABLE gato (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     nome VARCHAR(100),
     genero ENUM('M', 'F'), 
     raca VARCHAR(100),
     fkDono INT,
+    PRIMARY KEY (id, fkDono),
     CONSTRAINT fkDonoGato FOREIGN KEY (fkDono) REFERENCES usuario(idUsuario)
 );
 
 CREATE TABLE status_gato (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     fkGato INT,
     fkDono INT,
     fome INT,  -- valor que representa o nível de fome (pode ser de 0 a 100, por exemplo)
     sede INT,  -- valor que representa o nível de sede (pode ser de 0 a 100, por exemplo)
     dtHora TIMESTAMP,
     status_vida ENUM('VIVO', 'MORTO') DEFAULT 'VIVO',
+    PRIMARY KEY (id, fkGato, fkDono),
     CONSTRAINT fkGatoStatus FOREIGN KEY (fkGato) REFERENCES gato(id),
     CONSTRAINT fkDonoStatus FOREIGN KEY (fkDono) REFERENCES usuario(idUsuario)
 ); 
